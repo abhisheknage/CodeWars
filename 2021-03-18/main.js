c2a58063c708bcf63c0eb2845a49c834af63c8e7
Abhishek Nage <anage@deakin.edu.au>
2021-03-19 14:12:57 -0600

    Push work for 18-Mar

---
+++ 2021-03-18/main.js
@@ -0,0 +1,8 @@
+// Convert a string to an array
+// Write a function to split a string and convert it into an array of words. For example:
+
+function stringToArray(string) {
+  // code code code
+  return string.split(" ");
+}
+console.log(stringToArray("Hi my name is Abhishek"));
